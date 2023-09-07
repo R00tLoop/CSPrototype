556ed02f65371ee4f898681a67c342025ef37269
R00tCake <edwardcowap@gmail.com>
2023-09-07 23:11:54 +0100

    Commit to avoid version errors
    Been navigating the jxmapviewer2 unsuccessfully, using tutorial available on waybackmachine for guidance, will need to rethink this as library is not supported any more and is too old. Google maps API may be necessary.

---
+++ src/MapMain.java
@@ -0,0 +1,14 @@
+
+import org.jxmapviewer.JXMapViewer;
+import org.jxmapviewer.OSMTileFactoryInfo;
+import org.jxmapviewer.viewer.DefaultTileFactory;
+import org.jxmapviewer.viewer.GeoPosition;
+import org.jxmapviewer.viewer.TileFactoryInfo;
+
+
+public class MapMain
+{
+
+
+
+}
